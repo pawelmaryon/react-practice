28eba7f501a435517422740b573ccf25775f2639
P Maryon <maryonpawel@gmail.com>
2022-03-02 17:13:52 +0000

    Expenses component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+import Expenses from "./components/Expenses";
+
+function App(props) {
   const expenses = [
     {
       date: new Date(2022, 3, 2 ),
@@ -25,10 +26,7 @@ function App() {
   ]
   return (
     <div>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount}  date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount}  date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount}  date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount}  date={expenses[3].date}></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
